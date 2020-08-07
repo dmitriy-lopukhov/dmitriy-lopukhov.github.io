e1b1b2f76ab720007bd005c7c87dc8f03b314ed6
Dmitriy Lopukhov <dmitriy@admins-MacBook-Pro.local>
2020-08-07 22:43:38 +0200

    Updates

--- component---src-pages-404-tsx-e54cc6fb1c941bb66917.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"i6+/":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("9Dj+"),r=n("H8eV"),c=n("0X8b");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(r.a,{title:"404: Not found"}),l.a.createElement(c.f,null,l.a.createElement(c.a,null,l.a.createElement(c.b,null,l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))))}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-e54cc6fb1c941bb66917.js.map
